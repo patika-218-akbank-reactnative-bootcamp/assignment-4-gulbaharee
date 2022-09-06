e139cec31cad328612dca967256ff14ff2d1fca5
Gülbahar Erol <ggulbaharerol@gmail.com>
2022-09-06 21:23:35 +0300

    pages created

---
+++ movie_app/src/pages/Theme.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View,Text } from 'react-native';
+
+const Theme = ()=>{
+
+    return(
+        <View>
+            <Text>Theme</Text>
+        </View>
+    );
+}
+
+export default Theme;
---
+++ movie_app/src/pages/UserEdit.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View,Text } from 'react-native';
+
+const UserEdit = ()=>{
+
+    return(
+        <View>
+            <Text>UserEdit</Text>
+        </View>
+    );
+}
+
+export default UserEdit;
