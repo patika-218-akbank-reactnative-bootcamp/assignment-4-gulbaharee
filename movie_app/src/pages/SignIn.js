40f3b4a54a7a434daec2c8135671964706c07328
Gülbahar Erol <ggulbaharerol@gmail.com>
2022-09-07 21:32:28 +0300

    registered user data received, login added

--- movie_app/src/pages/SignIn.js
+++ movie_app/src/pages/SignIn.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   View,
   Text,
@@ -9,20 +9,54 @@ import {
   Button,
 } from 'react-native';
 import {useNavigation} from '@react-navigation/native';
+import axios from 'axios';
 
 const screen_Height = Dimensions.get('screen').height;
 const screen_Width = Dimensions.get('screen').width;
 
 const SignIn = () => {
   const {navigate} = useNavigation();
+
+  const [users, setUser] = useState('');
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = () => {
+    axios.get('http://localhost:3000/users').then(response => {
+      setUser(response.data);
+    });
+  };
+
+  const handleSubmit = e => {
+      
+    const userIndex = users.findIndex(user => user.username === usernameInput);
+    if (users[userIndex].password == passwordInput) {
+      console.log('login');
+    } else {
+      console.log('error');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.headerText}>Sign In</Text>
       <View style={styles.signIn}>
-        <TextInput style={styles.inputs} placeholder="username" />
-        <TextInput style={styles.inputs} placeholder="password" />
+        <TextInput
+          style={styles.inputs}
+          placeholder="username"
+          onChangeText={text => (usernameInput = text)}
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.inputs}
+          placeholder="password"
+          onChangeText={text => (passwordInput = text)}
+          autoCapitalize="none"
+        />
         {/* <Pressable><Text style={styles.button}>Sign In</Text></Pressable> */}
-        <Button title="Sign In" onPress={()=>navigate('Home')}/>
+        <Button title="Sign In" onPress={handleSubmit} />
         <Button
           title="Do not have an account?"
           onPress={() => navigate('SignUp')}
